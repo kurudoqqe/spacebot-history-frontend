f9ebb4dd6117451757826f28c45df4f7db47eb0b
kurudoqqe <vadim.firedog@gmail.com>
2025-01-10 03:52:15 +0300

    add moon crystal to Mine

---
+++ src/components/MoonCrystal.vue
@@ -0,0 +1,46 @@
+<script setup>
+import MoonCrystal from "../assets/images//moon/moon-crystal.png"
+
+const props = defineProps({
+  size: {
+    type: String,
+    required: true,
+  }
+})
+
+const enlargeCrystal = (target) => {
+  target.classList.add("clicked");
+  setTimeout(() => {
+    target.classList.remove("clicked");
+  }, 100)
+}
+</script>
+
+<template>
+  <img :src="MoonCrystal" alt="" :class="props.size"
+  @click="event => {enlargeCrystal(event.target)}">
+</template>
+
+<style scoped>
+img {
+  transition: transform 0.1s ease-in-out;
+  &.small {
+    width: 5.5rem;
+    height: 6.5rem;
+  }
+
+  &.medium {
+    width: 9.5rem;
+    height: 12.5rem;
+  }
+
+  &.large {
+    width: 12.5rem;
+    height: 17.5rem;
+  }
+
+  &.clicked {
+    transform: scale(1.35);
+  }
+}
+</style>
--- src/views/MineView.vue
+++ src/views/MineView.vue
@@ -2,6 +2,7 @@
 import {ref} from "vue";
 import MenuIcon from "@/components/icons/menu.svg"
 import SidebarComponent from "@/components/SidebarComponent.vue";
+import MoonCrystal from "@/components/MoonCrystal.vue";
 
 const isSidebarActive = ref(false)
 
@@ -17,11 +18,15 @@ const toggleSidebar = () => {
     <img :src="MenuIcon" alt=""
          class="menu-icon" @click="toggleSidebar"
          v-if="!isSidebarActive">
+    <MoonCrystal class="crystal" size="medium"/>
   </main>
 </template>
 
 <style scoped>
 main {
+  position: relative;
+  height: fit-content;
+  margin: 0 auto;
   background-image: url("../assets/images/mine-background.png");
   background-size: cover;
   background-position: center;
@@ -32,5 +37,12 @@ main {
   margin: 1.5rem 0 0 1.5rem;
   width: 1.75rem;
   height: 1.75rem;
+}
+
+.crystal {
+  position: absolute;
+  top: 50%;
+  left: 35%;
+  z-index: 0;
 }
 </style>

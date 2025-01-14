6b2b956ae864ebe530bb9b1b27260db6566c426a
kurudoqqe <vadim.firedog@gmail.com>
2025-01-14 03:47:54 +0300

    add new section to shop

---
+++ src/components/ShopSectionComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    asd
+  </section>
+</template>
+
+<style scoped>
+section {
+  margin-top: 5rem;
+  min-height: 5rem;
+  border-top: 1px solid var(--text-color);
+  border-bottom: 1px solid var(--text-color);
+}
+</style>
--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -4,6 +4,7 @@ import MenuIcon from "@/components/icons/menu.svg";
 import {useCrystalCountStore} from "@/store/crystalCount.js"
 import SidebarComponent from "@/components/SidebarComponent.vue";
 import CrystalCountComponent from "@/components/CrystalCountComponent.vue";
+import ShopSectionComponent from "@/components/ShopSectionComponent.vue";
 
 const isSidebarActive = ref(false);
 
@@ -18,23 +19,49 @@ const store = useCrystalCountStore();
 <template>
   <main>
     <SidebarComponent :toggle-sidebar="toggleSidebar" :is-sidebar-active="isSidebarActive"/>
-    <img :src="MenuIcon" alt=""
-         class="menu-icon" @click="toggleSidebar"
-         v-if="!isSidebarActive">
-    <CrystalCountComponent size="large" :count="0"/>
+    <header>
+      <img :src="MenuIcon" alt=""
+           class="menu-icon" @click="toggleSidebar"
+           v-if="!isSidebarActive">
+      <h1>Магазин</h1>
+      <CrystalCountComponent size="medium" :count="store.crystalCount"/>
+    </header>
+    <article>
+      <ShopSectionComponent/>
+      <ShopSectionComponent/>
+    </article>
   </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "../assets/variables.scss";
 
 main {
   height: fit-content;
   margin: 0 auto;
   background-color: var(--default-color);
+
+  header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    > h1 {
+      @include variables.capital-text;
+      color: var(--text-color);
+      text-shadow: var(--text-shadow);
+      font-size: 2rem;
+    }
+  }
+
+  article {
+    display: flex;
+    flex-direction: column;
+    padding: 0 1.5rem;
+  }
 }
 
 .menu-icon {
-  margin: 1.5rem 0 0 1.5rem;
   width: 1.75rem;
   height: 1.75rem;
 }
